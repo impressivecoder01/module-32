4936212086106b9393b981b518e04811e2983716
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-04 02:36:03 +0600

    class six added

--- classFive.js
+++ classFive.js
@@ -1,19 +1,19 @@
-const loadPost = () => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json())
-    .then(data => {
-        // console.log(data)
-        disPlayData(data)
-    })
-}
+// const loadPost = () => {
+//     fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then(res => res.json())
+//     .then(data => {
+//         // console.log(data)
+//         disPlayData(data)
+//     })
+// }
 
-const disPlayData = (posts) => {
-   const postContainer = document.getElementById('post-container')
+// const disPlayData = (posts) => {
+//    const postContainer = document.getElementById('post-container')
    
-    posts.forEach(post => {
-        console.log(post.title)
-        const li = document.createElement('li')
-        li.innerText = post.title
-        postContainer.appendChild(li)
-    })
-}
+//     posts.forEach(post => {
+//         console.log(post.title)
+//         const li = document.createElement('li')
+//         li.innerText = post.title
+//         postContainer.appendChild(li)
+//     })
+// }
---
+++ classSix.js
@@ -0,0 +1,25 @@
+const loadPost = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => {
+        // console.log(data)
+        disPlayData(data)
+    })
+}
+
+const disPlayData = (posts) => {
+   const postContainer = document.getElementById('post-container')
+   postContainer.innerHTML = ''
+    posts.forEach(post => {
+        const postCard = document.createElement('div');
+        postCard.innerHTML = `
+        <div  class="post-card">
+            <h1>${post.title}</h1>
+            <p>${post.body}</p>
+
+        </div>
+        `
+        postContainer.appendChild(postCard)
+    })
+}
+loadPost()
