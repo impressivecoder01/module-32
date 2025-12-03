75bf1e8c86c022d8b61082b0e93b0ad7eac685dc
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-04 00:54:07 +0600

    class four added

--- classFour.js
+++ classFour.js
@@ -1,1 +1,15 @@
 // recap fetch() & send and console each data
+const loadPost = ()=> {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        disPlayPost(data)
+    })
+}
+
+const disPlayPost = (posts) => {
+    posts.forEach(post => {
+        console.log(post)
+    })
+}
