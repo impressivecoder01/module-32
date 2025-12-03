245633a62ac7ae4509f5e73a1f79e7e94c892436
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-04 00:32:11 +0600

    class Four

---
+++ classFour.js
@@ -0,0 +1,1 @@
+// recap fetch() & send and console each data
--- classThree.js
+++ classThree.js
@@ -2,7 +2,10 @@
 // fetch('https://jsonplaceholder.typicode.com/todos/1')
 //       .then(response => response.json())
 //       .then(json => console.log(json))
-
-fetch('https://jsonplaceholder.typicode.com/todos/1')
+const loadData = ()=>{
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
 .then((res => res.json()))
-.then(data => console.log(data))      
+.then(data => console.log(data))    
+}
+
+  
