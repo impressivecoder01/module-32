1f286d023e1182ae4587bbf6ac6ef522b2d5fcb2
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-04 00:25:56 +0600

    classThree

---
+++ classThree.js
@@ -0,0 +1,8 @@
+//load json data using fetch function
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+.then((res => res.json()))
+.then(data => console.log(data))      
